bda1d1afea6417d75db030558459a718be60f32a
Ihor Melnyk <92684157+Ihor-Melnyk@users.noreply.github.com>
2022-07-02 15:08:31 +0300

    add files

---
+++ src/components/FriendListItem/index.jsx
@@ -0,0 +1,13 @@
+
+export const FriendListItem = ({ avatar, name, isOnline }) => (
+<li className="item">
+      <span className="status">{isOnline}</span>
+      <img className="avatar" src={avatar} alt="User avatar" width="48" />
+      <p className="name">{name}</p>
+</li>
+)
+
+// FriendListItem.propTypes = {
+//       // avatar: PropTypes.string,
+      
+// }
